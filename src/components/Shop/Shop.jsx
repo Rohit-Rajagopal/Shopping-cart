f711591e7fa2b2995ae4917b77d30f919c0b128a
Rohit <rohitrajagopal04@gmail.com>
2025-11-21 23:04:14 +0530

    added functionality to fetch data from fakestore api

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,21 +2,39 @@ import { useState, useEffect } from "react";
 import ItemCard from "../ItemCard/ItemCard";
 import styles from "./Shop.module.css";
 
+const useFetch = (url) => {
+    const [ data, setData ] = useState(null);
+    const [ loading, setLoading ] = useState(true);
+    const [ error, setError ] = useState(false);
+
+    useEffect(() => {
+        fetch(url)
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error("server error");
+                }
+                return response.json();
+            })
+            .then((response) => setData(response))
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+    return { data, loading, error };
+};
+
 function Shop() {
+    const { data, loading, error } = useFetch("https://fakestoreapi.com/products");
+
+    if (loading) return <p>Loading...</p>;
+
+    if (error) return <p>Server error: {error}</p>
 
     return (
         <div className={styles.itemsDisplay}>
-            <ItemCard name="shirt" price="5" />
-            <ItemCard name="shirt" price="5" />
-            <ItemCard name="shirt" price="5" />
-            <ItemCard name="shirt" price="5" />
-            <ItemCard name="shirt" price="5" />
-            <ItemCard name="shirt" price="5" />
-            <ItemCard name="shirt" price="5" />
-            <ItemCard name="shirt" price="5" />
-            <ItemCard name="shirt" price="5" />
-            <ItemCard name="shirt" price="5" />
-            <ItemCard name="shirt" price="5" />
+            {
+                data.map((item) => <ItemCard name={item.title} price={item.price} />)
+            }
         </div>
     )
 }
